670b34b70ad4e3f43a36c89de20b66baf65802be
NishantNN <101966468+NishantNN@users.noreply.github.com>
2024-02-06 16:59:55 +0545

    Add files via upload

---
+++ Creator/creator_index.js
@@ -0,0 +1,39 @@
+function toggleProfile() {
+    // Get the element with the id "profile_on"
+    let activeProfile = document.getElementById("profile_on");
+
+    // Toggle the "active" class
+    activeProfile.classList.toggle("active");
+}
+
+let UserCreds = JSON.parse(sessionStorage.getItem('user-creds'));
+let UserInfo = JSON.parse(sessionStorage.getItem('user-info'));
+
+let user_type = document.getElementById('user_type');
+let fullname = document.getElementById('fullname');
+let mail = document.getElementById('mail');
+let phoneno = document.getElementById('phoneno');
+let changepassword = document.getElementById('changepassword');
+let LogoutBtn = document.getElementById('LogoutBtn');
+
+
+let Logout = () => {
+    sessionStorage.removeItem("user-creds");
+    sessionStorage.removeItem("user-info");
+    window.location.href = '/login.html';
+}
+
+let CheckCred = () => {
+    if (!sessionStorage.getItem("user-creds")) {
+        window.location.href = '/login.html'
+    }
+    else {
+        user_type.innerText = `${UserInfo.UserType}`;
+        fullname.innerText = `${UserInfo.Fullname}`;
+        phoneno.innerText = `${UserInfo.PhoneNo}`;
+        mail.innerText = `${UserCreds.email}`;
+    }
+}
+
+window.addEventListener('load', CheckCred);
+LogoutBtn.addEventListener('click', Logout)
